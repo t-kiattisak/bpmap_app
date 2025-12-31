89a4fd747a295564ebc1e1726568563dfbb37744
kiattisak jomram <t.kiattisak.j@gmail.com>
2025-12-31 16:16:16 +0700

    feat: Update Android package name, iOS bundle identifiers, and Firebase configuration.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.bpmap_app"
+    namespace = "com.bpmap.disaster"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -22,7 +22,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.bpmap_app"
+        applicationId = "com.bpmap.disaster"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
